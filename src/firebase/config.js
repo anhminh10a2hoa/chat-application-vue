c854dc77e0e84ce0f2bdffb76636d764589dcf5d
Hoang Anh Minh <anhminh10a2hoa@gmail.com>
2021-04-23 00:57:25 +0300

    login and sign up

---
+++ src/firebase/config.js
@@ -0,0 +1,20 @@
+import firebase from 'firebase/app'
+import 'firebase/firestore'
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDTqnZuCjGpdK5aDiCgU3ZA8jd8AsjcZiw",
+  authDomain: "live-chat-983f6.firebaseapp.com",
+  projectId: "live-chat-983f6",
+  storageBucket: "live-chat-983f6.appspot.com",
+  messagingSenderId: "903723737985",
+  appId: "1:903723737985:web:35954f510d7496b4fb7e80",
+  measurementId: "G-QQ0CYP0WMY"
+};
+
+// init firebase
+firebase.initializeApp(firebaseConfig)
+
+const projectFirestore = firebase.firestore()
+const timestamp = firebase.firestore.FieldValue.serverTimestamp()
+
+export { projectFirestore, timestamp }
--- src/views/Welcome.vue
+++ src/views/Welcome.vue
@@ -1,18 +1,47 @@
 <template>
   <div class="welcome container">
     <p>Welcome</p>
+    <LoginForm />
   </div>
 </template>
 
 <script>
-export default {
+import SignupForm from '../components/SignupForm.vue'
+import LoginForm from '../components/LoginForm.vue'
 
+export default {
+  components: { SignupForm, LoginForm }
 }
 </script>
 
 <style>
   .welcome {
-    text-align: center;
-    padding: 20px 0;
-  }
+  text-align: center;
+  padding: 20px 0;
+}
+.welcome form {
+  width: 300px;
+  margin: 20px auto;
+}
+.welcome label {
+  display: block;
+  margin: 20px 0 10px;
+}
+.welcome input {
+  width: 90%;
+  padding: 10px;
+  border-radius: 20px;
+  border: 1px solid #eee;
+  outline: none;
+  color: #999;
+  margin: 10px auto;
+}
+.welcome span {
+  font-weight: bold;
+  text-decoration: underline;
+  cursor: pointer;
+}
+.welcome button {
+  margin: 20px auto;
+}
 </style>
